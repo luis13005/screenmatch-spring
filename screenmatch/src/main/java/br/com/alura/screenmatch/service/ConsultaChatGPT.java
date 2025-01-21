c9ac00b72d7ae8a6bab6ba256bda8b0fdf8e157a
Luis-Fernando <105455585+luis13005@users.noreply.github.com>
2025-01-21 13:59:22 -0400

    iniciando ORM

--- screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
-    private static String API_KEY = "*****ColoqueSuaApiAqui****";
+    private static String API_KEY = "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";
 
     public static String Consultar(String txt){
         System.out.println("API: "+API_KEY);
